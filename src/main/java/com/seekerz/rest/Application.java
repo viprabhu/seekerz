5660076e3531337cb5db595898cd35644269194f
Vignesh Prabhu <vignesh.prabhu@gmail.com>
2018-01-27 13:35:28 +0100

    Fourth commit

--- src/main/java/com/seekerz/rest/Application.java
+++ src/main/java/com/seekerz/rest/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World - Spring Boot REST World";
+        return "Hello Docker World - Spring Boot REST example";
     }
 
     public static void main(String[] args) {
